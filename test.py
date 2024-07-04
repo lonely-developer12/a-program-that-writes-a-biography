c8f2f87b40ff521cf77bbd01412b176f2215a49e
lonely-developer12 <emirbuzdic@gmail.com>
2024-07-04 18:21:30 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+your_full_name_and_surname =input('please enter your full name and surname: ')
+date_of_birth =input('please enter your date of birth: ')
+birthplace = input('please enter the correct place of birth: ')
+placeofresidence = input('please provide the exact name of the residence and the exact address: ')
+highschool=input('please indicate your completed high school: ')
+theacademyyouattended = input('enter the name of the academy you attended: ') 
+skills = input('please list your skills: ')
+print('biography')
+
+print('My name is ' + your_full_name_and_surname + 'I live in '+ placeofresidence + 'I was born '+ birthplace +'I graduated high school '+
+highschool+ ' now I am a student of the Informatics Academy ' + theacademyyouattended + 'I have now completed '+ skills + ' I am very motivated and goal oriented in my free time I also like programming and reading')
+
